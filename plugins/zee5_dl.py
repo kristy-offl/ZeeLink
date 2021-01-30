477d7605e0b4af0b9009fa42632600c646a26cf5
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-01-30 08:47:16 +0530

    Update zee5_dl.py

--- plugins/zee5_dl.py
+++ plugins/zee5_dl.py
@@ -219,7 +219,8 @@ async def zee5_execute(bot, update):
         youtube_dl_url = zee5_capture.url
         
         linksplit = update.message.reply_to_message.text.split("/")
-        videoname = linksplit[+5]
+        #videoname = linksplit[+5]
+        custom_file_name = linksplit[+5]
         #logger.info(videoname)
         logger.info(custom_file_name)
         
