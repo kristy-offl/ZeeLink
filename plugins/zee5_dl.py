c6747ab029e8c995105002d2d6e721081615ebc9
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-01-31 21:32:30 +0530

    Update zee5_dl.py

--- plugins/zee5_dl.py
+++ plugins/zee5_dl.py
@@ -220,7 +220,7 @@ async def zee5_execute(bot, update):
         
         linksplit = update.message.reply_to_message.text.split("/")
         #videoname1 = linksplit[+5]
-        videoname = r2["title"]
+        videoname = file_name
         #custom_file_name = linksplit[+5]
         logger.info(videoname)
         #logger.info(custom_file_name)
