1bd06a5b57a8c28769f0b4fffb9676dcd7f31a5d
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-01 22:15:50 +0530

    Create cb_button1.py

---
+++ plugins/cb_button1.py
@@ -0,0 +1,14 @@
+import pyrogram
+
+from plugins.zee5_dl import zee5_execute
+#from plugins.zee5movies import zee5_execute1
+
+
+@pyrogram.Client.on_callback_query()
+async def formatbuttons(bot, update):
+       
+    if "|" in update.data:
+        await zee5_execute(bot, update)
+        
+    elif "closeformat2" in update.data:     
+        await update.message.delete()
