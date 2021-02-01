fca3bf09a109f62d8af4d4ad6fa7868d8a1fb3fd
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-01 22:27:28 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -5,7 +5,7 @@ from plugins.zee5movies import zee5_execute1
 
 
 @pyrogram.Client.on_callback_query()
-async def formatbuttons(bot, update):
+async def formatbuttons1(bot, update):
        
     if "|" in update.data:
         await zee5_execute1(bot, update)
