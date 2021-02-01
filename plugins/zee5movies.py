fa84a4f02a1ae2916af864acbd738b5988046b3d
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-01 20:58:21 +0530

    Update zee5movies.py

--- plugins/zee5movies.py
+++ plugins/zee5movies.py
@@ -41,7 +41,7 @@ from plugins.helpers import(
 
 
 
-@Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
+@Client.on_message(filters.private & filters.regex(pattern=".*https://www.zee5.com/movies/.*"))
 async def zee5_capture(bot, update):
 
     if update.from_user.id in Config.BANNED_USERS:
