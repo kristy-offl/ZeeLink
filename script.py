bd116db74fad2d713cb64f7484dfcc20d5a8dfad
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-07 01:09:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -54,6 +54,6 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     NO_THUMB = "So Sad 😢 No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "description"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @JAsuran ❤️</b>"
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
